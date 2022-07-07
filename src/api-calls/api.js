aee533a4c53d2cf2056951d77c3ff06c3b7dcaf2
Jag <jag@loveforweb.com>
2022-07-07 11:20:07 +0100

    api endpoint update

--- src/api-calls/api.js
+++ src/api-calls/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 export const API_STALE_TIMEOUT = 3600000;
-const API_BASE_URL = 'https://restcountries.eu/rest/v2';
+const API_BASE_URL = 'https://restcountries.com/v2';
 
 export const allCountriesAPI = async () => {
   const { data } = await axios.get(
-    `${API_BASE_URL}/all?fields=name;capital;region;flag;population;alpha3Code`
+    `${API_BASE_URL}/all?fields=name,capital,region,flag,population,alpha3Code`
   );
   return data;
 };
 
 export const countryDataAPI = async (alpha3Code) => {
   const { data } = await axios.get(
-    `${API_BASE_URL}/alpha/${alpha3Code}?fields=name;flag;population;flag;region;capital;nativeName;subregion;topLevelDomain;currencies;languages;borders`
+    `${API_BASE_URL}/alpha/${alpha3Code}?fields=name,flag,population,flag,region,capital,nativeName,subregion,topLevelDomain,currencies,languages,borders`
   );
 
   return data;
@@ -19,7 +19,7 @@ export const countryDataAPI = async (alpha3Code) => {
 
 export const borderAPI = async (borderCodeParams) => {
   const { data } = await axios.get(
-    `${API_BASE_URL}/alpha?codes=${borderCodeParams}&fields=name;alpha3Code;population;region;capital;flag;alpha3Code`
+    `${API_BASE_URL}/alpha?codes=${borderCodeParams}&fields=name,alpha3Code,population,region,capital,flag,alpha3Code`
   );
   return data;
 };
